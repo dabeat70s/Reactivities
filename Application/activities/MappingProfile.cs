bb916b22edef0425d1b5a87eee9e643ec323775c
Tony Adams <atny1@outlook.com>
2020-10-05 02:16:19 +0100

    sect 16 photoInCore complete

--- Application/activities/MappingProfile.cs
+++ Application/activities/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -9,8 +10,9 @@ namespace Application.activities
         {
             CreateMap<Activity, ActivityDTO>();
             CreateMap<UserActivity, AttendeeDto>()
-            .ForMember(destinationMember=>destinationMember.Username, o=> o.MapFrom(sourceMember=>sourceMember.AppUser.UserName))
-            .ForMember(d=> d.DisplayName, o=>o.MapFrom(s => s.AppUser.DisplayName)) ;
+            .ForMember(destinationMember=>destinationMember.Username, o=> o.MapFrom(sourceMember=>sourceMember.AppUser.UserName)) //o = options
+            .ForMember(d=> d.DisplayName, o=>o.MapFrom(s => s.AppUser.DisplayName))
+            .ForMember(d=>d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url)) ;
         }
         
     }
